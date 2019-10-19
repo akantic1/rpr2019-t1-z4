7bf6f8fececff0c97c49488739e83e82c1496f95
akantic1 <akantic1@etf.unsa.ba>
2019-10-19 20:31:41 +0200

    commitane potrebne funkcije

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -2,7 +2,19 @@ package ba.unsa.etf.rpr;
 
 public class Korpa {
     Artikl[] korpaArtikala = new Artikl[50];
+    int brojArtikala = 0;
     public boolean dodajArtikl(Artikl a) {
+        if(brojArtikala == 50){
+            System.out.println("U korpi se ne može nalaziti više od 50 artikala");
+            return false;
+        }
+        else{
+            korpaArtikala[brojArtikala] = a;
+            brojArtikala++;
+            return true;
+        }
+
+
     }
 
     public Artikl[] getArtikli() {
@@ -10,8 +22,22 @@ public class Korpa {
     }
 
     public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl ar = null;
+        for (int i = 0; i < brojArtikala; i++) {
+            if (korpaArtikala[i].getKod().equals(kod))
+                ar = korpaArtikala[i];
+            for (int j = i; j < brojArtikala; j++)
+                korpaArtikala[j] = korpaArtikala[j + 1];
+            brojArtikala--;
+        }
+        return ar;
     }
 
     public int dajUkupnuCijenuArtikala() {
+        int ukupnaCijena = 0;
+        for(Artikl ar : korpaArtikala){
+            ukupnaCijena += ar.getCijena();
+        }
+        return ukupnaCijena;
     }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -3,13 +3,31 @@ package ba.unsa.etf.rpr;
 public class Supermarket {
 
 
+    Artikl[] artikli =new Artikl[100];
+    int brojArtikala = 0;
+
     public Artikl[] getArtikli() {
+        return artikli;
     }
 
     public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl ar = null;
+        for (int i = 0; i < brojArtikala; i++) {
+            if (artikli[i].getKod().equals(kod))
+                ar = artikli[i];
+            for (int j = i; j < brojArtikala; j++)
+                artikli[j] = artikli[j + 1];
+            brojArtikala--;
+        }
+        return ar;
     }
 
     public void dodajArtikl(Artikl a) {
-
+        if(brojArtikala == 100)
+            System.out.println("U supermarketu se ne može nalaziti više od 100 artikala");
+        else{
+            artikli[brojArtikala] = a;
+            brojArtikala++;
+        }
     }
 }
